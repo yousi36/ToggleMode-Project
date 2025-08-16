d66c4c9071dfb46368d97fd38ab13b20d7dac378
yousi36 <muhammadyousafworkplace1012@gmail.com>
2025-08-16 20:06:25 +0500

    project initialize

---
+++ src/Component/ToggleMode.jsx
@@ -0,0 +1,9 @@
+export function ToggleMode (){
+    return (
+        <>
+        <h1>Dark Light Mode Website</h1>
+        <h4>Hello! My React V19 Fan</h4>
+        <button>Switch to Dark Mode </button>
+        </>
+    )
+}
